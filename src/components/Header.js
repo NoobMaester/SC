24c5f49a4d6fc102c16eacc22a1ac2b27f95581e
NoobMaester <joshstilez@gmail.com>
2021-09-30 20:06:19 +0200

    Button styled commponent added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
-import {StyledHeader} from './styles/Header.styled';
+import {Logo, Nav, StyledHeader} from './styles/Header.styled';
 import { Container } from './styles/Container.styled';
+import { Button } from './styles/Button.styled';
 
 export default function Header (){
     return (
         <StyledHeader>
 
             <Container>
-                <h1>Hubble</h1>
+                <Nav>
+                    <Logo/>
+                    <Button>Try It Free</Button>
+                </Nav>
             </Container>
             
         </StyledHeader>
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -4,3 +4,12 @@ export const StyledHeader = styled.header`
     background-color: ${({theme}) => theme.colors.header};
     padding: 40px;
 `
+export const Nav = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 40px;
+`
+export const Logo = styled.img`
+
+`
