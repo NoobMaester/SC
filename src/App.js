ea9a3d73f2a307478e5f46b220032e7c9c7f0361
NoobMaester <joshstilez@gmail.com>
2021-09-30 19:39:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { ThemeProvider } from 'styled-components';
+import  Header  from './components/Header';
+import GlobalStyles from './components/styles/Global';
+import {Container } from './components/styles/Container.styled'
+
+const theme = {
+  colors:{
+    header:'#ebfbff',
+    body: '#fff',
+    footer: '#003333',
+  },
+}
+
+
+function App() {
+  return (
+    <ThemeProvider theme = {theme}>
+
+      <>
+        <GlobalStyles/>
+        <Header/>
+          <Container>
+            <h1>Hello World</h1>
+          </Container>
+      </>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
